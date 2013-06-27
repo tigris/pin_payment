50da9d9e96a9de965e87318fb3f380e482129792
Danial Pearce <git@tigris.id.au>
2013-06-27 10:43:42 +1000

    Add test for create charge with card hash

--- test/test_pin_charge.rb
+++ test/test_pin_charge.rb
@@ -17,4 +17,25 @@ class TestPinCharge < MiniTest::Unit::TestCase
     charge = PinPayment::Charge.create(customer_token: 'cus__03Cn1lSk3offZ0IGkwpCg', amount: 1000)
     assert_equal true, charge.success?
   end
+
+  def test_create_charge_with_card_hash
+    FakeWeb.register_uri(:post, 'https://test-api.pin.net.au/1/charges', body: fixtures['responses']['charge']['create_with_card'])
+    card_hash = {
+      number:           5520000000000000,
+      expiry_month:     5,
+      expiry_year:      2014,
+      cvc:              123,
+      name:             'Roland Robot',
+      address_line1:    '42 Sevenoaks St',
+      address_city:     'Lathlain',
+      address_postcode: 6454,
+      address_state:    'WA',
+      address_country:  'Australia'
+    }
+    charge = PinPayment::Charge.create(amount: 400, currency: 'AUD', description: 'test charge', email: 'roland@pin.net.au', ip_address: '203.192.1.172', card: card_hash)
+    assert_equal true, charge.success?
+    assert_kind_of PinPayment::Card, charge.card
+    assert_kind_of String, charge.card.token
+    assert charge.card.token.length > 0
+  end
 end
