257f00be4d597a3909def9ce42ebe62df78188f9
Giancarlo Salamanca <giancarlo.salamanca@envato.com>
2014-05-27 09:27:54 +1000

    Add total_fees to Charge

--- lib/pin_payment/charge.rb
+++ lib/pin_payment/charge.rb
@@ -1,7 +1,7 @@
 module PinPayment
   class Charge < Base
-    attr_accessor :token,  :amount,  :currency,  :description,  :email,  :ip_address,  :created_at,  :card,  :customer,  :success
-    protected     :token=, :amount=, :currency=, :description=, :email=, :ip_address=, :created_at=, :card=, :customer=, :success=
+    attr_accessor :token,  :amount,  :currency,  :description,  :email,  :ip_address,  :created_at,  :card,  :customer,  :success,  :total_fees
+    protected     :token=, :amount=, :currency=, :description=, :email=, :ip_address=, :created_at=, :card=, :customer=, :success=, :total_fees=
 
     # Uses the pin API to create a charge.
     #
@@ -55,7 +55,7 @@ module PinPayment
     protected
 
     def self.attributes
-      [:token, :amount, :currency, :description, :email, :ip_address, :created_at, :card, :customer, :success]
+      [:token, :amount, :currency, :description, :email, :ip_address, :created_at, :card, :customer, :success, :total_fees]
     end
   end
 end
