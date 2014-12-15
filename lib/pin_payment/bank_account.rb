75c2b86e651726f9a6960333809a0381cc581b56
Ruxton <ruxton@thefrontiergroup.com.au>
2014-12-15 10:43:16 +0800

    Initial commit with Marketplace BETA API support

---
+++ lib/pin_payment/bank_account.rb
@@ -0,0 +1,33 @@
+module PinPayment
+  class BankAccount < Base
+
+    attr_accessor :token,  :name,  :bsb,  :number, :bank_name
+    protected     :token=, :name=, :bsb=, :number=, :bank_name=
+
+    # Uses the pin API to create a bank account.
+    #
+    # @param [Hash] bank_account_data
+    # @option bank_account_data [String] :name *required*
+    # @option bank_account_data [String] :bsb *required*
+    # @option bank_account_data [String] :number *required*
+    # @return [PinPayment::BankAccount]
+    def self.create bank_account_data
+      attributes = self.attributes - [:token,:bank_name] # fix attributes allowed by POST API
+      options    = parse_options_for_request(attributes, bank_account_data)
+      response   = post(URI.parse(PinPayment.api_url).tap{|uri| uri.path = '/1/bank_accounts' }, options)
+      new(response.delete('token'), response)
+    end
+
+    # @return [Hash]
+    def to_hash
+      {}.tap{|h| self.class.attributes.each{|k| v = send(k) ; h[k] = v if v }}
+    end
+
+    protected
+
+    def self.attributes
+      [:token, :name, :bsb, :number, :bank_name]
+    end
+
+  end
+end
---
+++ lib/pin_payment/transfer.rb
@@ -0,0 +1,63 @@
+module PinPayment
+  class Transfer < Base
+
+    attr_accessor :description, :amount, :currency, :recipient, :token
+    protected     :description=, :amount=, :currency=, :recipient=, :token=
+
+    # Uses the pin API to create a transfer.
+    #
+    # @param [Hash] transfer_data
+    # @option transfer_data [String] :description *required*
+    # @option transfer_data [String] :amount *required*
+    # @option transfer_data [String] :currency *required* (Currenly only AUD is supported)
+    # @option transfer_data [String] :recipient *required* a token for a stored recipient
+    # @return [PinPayment::Transfer]
+    def self.create transfer_data
+      attributes = self.attributes - [:token] # fix attributes allowed by POST API
+      options    = parse_options_for_request(attributes, transfer_data)
+      response   = post(URI.parse(PinPayment.api_url).tap{|uri| uri.path = '/1/transfers' }, options)
+      new(response.delete('token'), response)
+    end
+
+    # Fetches all of your transfers using the pin API.
+    #
+    # @return [Array<PinPayment::Transfer>]
+    # TODO: pagination
+    def self.all
+      response = get(URI.parse(PinPayment.api_url).tap{|uri| uri.path = '/1/transfers' })
+      response.map{|x| new(x.delete('token'), x) }
+    end
+
+    # Fetches a transfer using the pin API.
+    #
+    # @param [String] token the recipient token
+    # @return [PinPayment::Transfer]
+    def self.find token
+      response = get(URI.parse(PinPayment.api_url).tap{|uri| uri.path = "/1/transfers/#{token}" })
+      new(response.delete('token'), response)
+    end
+
+    def line_items
+      response = self.class.get(URI.parse(PinPayment.api_url).tap{|uri| uri.path = "/1/transfers/#{token}/line_items" })
+      response.map{|x| LineItem.new(x.delete('token'),x) }
+    end
+
+    protected
+
+    def self.attributes
+      [:token, :description, :amount, :currency, :recipient]
+    end
+
+    class LineItem < Base
+      attr_accessor :type, :object, :amount, :token
+      protected     :type=, :object=, :amount=, :token=
+
+      protected
+
+      def self.attributes
+        [:token, :type, :object, :amount]
+      end
+    end
+
+  end
+end
